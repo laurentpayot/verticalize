f0e76707101f95bc14207a75270e1cd00624d4b7
Laurent Payot <laurent.payot@publiberty.com>
2023-09-25 16:13:23 +0200

    improved error messages

--- verticalize.js
+++ verticalize.js
@@ -4,7 +4,7 @@
 export const V = new Proxy(
     (...args) => args.reduce( (out, fn) => {
         if (typeof fn !== 'function')
-            throw new TypeError(`${fn} is not a function`)
+            throw new TypeError(`Verticalize: ${fn?.constructor.name ?? fn} value is not a function`)
         if (out instanceof Promise) {
             return out.then(fn._Vprop ? result => result[fn._Vprop] : fn)
         } else {
@@ -14,7 +14,7 @@ export const V = new Proxy(
     { get(_, prop) {
         const toFn = (...args) => out => {
             if (typeof out[prop] !== 'function')
-                throw new Error(`Verticalize pipe output has no method .${prop}()`)
+                throw new Error(`Verticalize: ${out?.constructor.name ?? out} value has no method .${prop}()`)
             return out[prop].apply(out, args)
         }
         toFn._Vprop = prop
