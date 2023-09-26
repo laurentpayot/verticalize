ce377aa8f899616176828fdbb9beb99bab5b34d9
Laurent Payot <laurent.payot@publiberty.com>
2023-09-26 12:36:27 +0200

    proper TS type

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,11 @@
-// Cannot use `...functions: Function[]` because `V .length` would create the following error:
-// Argument of type 'number' is not assignable to parameter of type 'Function'.
-export const V: (value: any, ...functions: any[]) => any
+type Fn = (...args: any[]) => any
+
+type LastReturnType<L extends Fn[]> = L extends [...any, infer Last extends Fn]
+    ? ReturnType<Last>
+    : never
+
+type GenericObject = Record<string, any>
+
+export const V:
+    (<Functions extends Fn[]>(value: any, ...functions: Functions) => LastReturnType<Functions>)
+    & GenericObject
