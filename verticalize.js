da8b4c2e81fde358cf22ea25be128d4d9ebadbe2
Laurent Payot <laurent.payot@publiberty.com>
2024-04-03 10:41:58 +0200

    condition simplification

--- verticalize.js
+++ verticalize.js
@@ -5,11 +5,9 @@ export const V = new Proxy(
   (...args) => args.reduce( (out, fn) => {
     if (typeof fn !== 'function')
       throw new TypeError(`Verticalize: ${fn?.constructor.name ?? fn} value is not a function`)
-    if (out instanceof Promise) {
+    if (out instanceof Promise)
       return out.then(fn._Vprop ? result => result[fn._Vprop] : fn)
-    } else {
-      return fn._Vprop ? out[fn._Vprop] : fn(out)
-    }
+    return fn._Vprop ? out[fn._Vprop] : fn(out)
   }),
   { get(_, prop) {
     const toFn = (...args) => out => {
